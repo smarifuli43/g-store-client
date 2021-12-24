11305570fa22d4f063a211ca69cb8f7c2be7659b
Md Ariful Islam <smarifulislami43@gmail.com>
2021-12-25 02:48:27 +0600

    firebase

---
+++ src/Auth/Login/Login.js
@@ -0,0 +1,123 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Header from '../../Shared/Header/Header';
+import { useForm } from 'react-hook-form';
+import googleLogo from '../../../img/google-color.svg';
+import useAuth from '../../../Hooks/useAuth';
+import PasswordReset from '../Reset/PasswordReset/PasswordReset';
+import { Alert } from 'react-bootstrap';
+import { useNavigate, useLocation } from 'react-router';
+
+
+const Login = () => {
+  const { signinWithEmailandPass, signInWithGoogle} =
+    useAuth();
+  const navigate = useNavigate()
+  const location = useLocation();
+  const [modalShow, setModalShow] = React.useState(false);
+
+  const { register, handleSubmit, reset, error } = useForm();
+  const onSubmit = (data) => {
+    handleEmailandPassSignin(data);
+  };
+  //  email signin
+  const handleEmailandPassSignin = (user) => {
+    signinWithEmailandPass(user.email, user.password, navigate, location);
+    reset();
+  };
+  const googleSignIn = () => {
+    signInWithGoogle( navigate, location);
+  };
+
+  const facebookSignIn = () => {
+    signInWithFacebook(navigate, location);
+  };
+
+  return (
+    <>
+      <Header />
+      <div className='container my-5'>
+        <div className='row d-flex justify-content-center '>
+          <div
+            className='col-10 col-md-8 col-lg-5 data-form'
+            style={{
+              border: '1px solid #ABABAB',
+              borderRadius: '4px',
+              padding: '30px 50px',
+            }}
+          >
+            <h3 className='mb-4' style={{ fontWeight: 600 }}>
+              Login
+            </h3>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <input
+                placeholder='Your Email'
+                type='email'
+                {...register('email')}
+                required
+              />
+              <input
+                placeholder='Password'
+                type='password'
+                {...register('password')}
+                required
+                className='mb-2'
+              />
+              <span className='my-0 py-0' style={{ fontSize: '15px' }}>
+                Forgot Password?
+                <button
+                  className='btn text-danger'
+                  onClick={() => setModalShow(true)}
+                >
+                  {' '}
+                  Reset Now
+                </button>
+              </span>
+              <PasswordReset
+                show={modalShow}
+                onHide={() => setModalShow(false)}
+              ></PasswordReset>
+              <button type='submit' className='btn-luxury w-100'>
+                Login
+              </button>
+            </form>
+            <p className='text-center mt-2 mb-0'>
+              Don't have an account?{' '}
+              <span>
+                {' '}
+                <Link to='/register' className='text-decoration-underline '>
+                  Register
+                </Link>{' '}
+              </span>
+            </p>
+
+            {error && (
+              <Alert variant='danger' className='mx-3 py-2'>
+                {error}
+              </Alert>
+            )}
+          </div>
+        </div>
+        <div className='row d-flex justify-content-center px-md-5'>
+          <div className='col-10 col-md-8 col-lg-5 text-center'>
+            <div className=' d-flex justify-content-center my-3 mx-2 align-items-center'>
+              <span className='hl'></span>
+              <h5>Or</h5>
+              <span className='hl'></span>
+            </div>
+            <button
+              className='d-flex justify-content-between bg-transparent rounded-pill p-2 w-100'
+              style={{ border: '1px solid #C7C7C7' }}
+              onClick={googleSignIn}
+            >
+              <img src={googleLogo} alt='' style={{ width: '28px' }} />
+              <span className='pe-1 pe-md-5'>Continue with Google</span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
---
+++ src/Hooks/useFirebase.js
@@ -0,0 +1,173 @@
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+  onAuthStateChanged,
+  signOut,
+  signInWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup,
+  sendPasswordResetEmail,
+} from 'firebase/auth';
+import { useEffect, useState } from 'react';
+import initializeAuthentication from '../Pages/Login/firebase/firebase.init';
+
+initializeAuthentication();
+
+const useFirebase = () => {
+  const [error, setError] = useState('');
+  const [user, setUser] = useState({});
+  const [success, setSuccess] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [admin, setAdmin] = useState(false);
+
+  const auth = getAuth();
+  const googleProvider = new GoogleAuthProvider();
+  
+
+  // register
+  const createNewUser = (email, password, name, navigate) => {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        setError('');
+        setUser(result.user);
+        setUserName(name);
+        saveUser(email, name, 'POST');
+        navigate('/home');
+      })
+      .catch((error) => {
+        setError(error.message);
+      });
+  };
+
+  const setUserName = (name) => {
+    updateProfile(auth.currentUser, {
+      displayName: name,
+    })
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  // signin with email and pass
+  const signinWithEmailandPass = (email, password, navigate, location) => {
+    setIsLoading(true);
+    signInWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        setUser(result.user);
+        const destination = location?.state?.from || '/';
+        navigate(destination);
+      })
+      .catch((error) => {
+        console.log(error.message);
+        setError(error.message);
+      })
+      .finally(() => setIsLoading(false));
+  };
+
+  // google signin
+  const signInWithGoogle = (navigate, location) => {
+    setIsLoading(true);
+
+    signInWithPopup(auth, googleProvider)
+      .then((result) => {
+        const user = result.user;
+        setUser(user);
+        saveUser(user.email, user.displayName, 'PUT');
+        const destination = location?.state?.from || '/';
+        navigate(destination);
+      })
+      .catch((error) => {
+        setError(error.message);
+      })
+      .finally(() => setIsLoading(false));
+  };
+
+  // // facebook signIn
+  // const signInWithFacebook = () => {
+  //   signInWithPopup(auth, facebookProvider)
+  //     .then((result) => {
+  //       const user = result.user;
+  //       setUser(user);
+  //       saveUser(user.email, user.displayName, 'PUT');
+  //     })
+  //     .catch((error) => {
+  //       setError(error.message);
+  //     });
+  // };
+
+  // password reset email
+  const passwordReset = (email) => {
+    sendPasswordResetEmail(auth, email)
+      .then((result) => {
+        setError('');
+        setSuccess(true);
+      })
+      .catch((error) => {
+        setSuccess('');
+        setError(error.message);
+      });
+  };
+
+  // logout
+  const logout = (navigate) => {
+    signOut(auth).then(() => {
+      setUser({});
+      navigate('/home');
+    });
+  };
+
+  // observe the user
+  useEffect(() => {
+    const unsubscibed = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser({});
+      }
+      setIsLoading(false);
+    });
+    return () => unsubscibed;
+  }, [auth]);
+
+  // see whether admin or not
+  useEffect(() => {
+    fetch(`http://localhost:5000/users/${user.email}`)
+      .then((res) => res.json())
+      .then((data) => setAdmin(data.admin));
+  }, [user.email]);
+
+  // save user to the database
+  const saveUser = (email, name, method) => {
+    const user = { email, name };
+    fetch('http://localhost:5000/users', {
+      method: method,
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify(user),
+    }).then();
+  };
+
+  return {
+    user,
+    admin,
+    setUser,
+    error,
+    createNewUser,
+    logout,
+    signinWithEmailandPass,
+    signInWithGoogle,
+    passwordReset,
+    success,
+    setSuccess,
+    setError,
+    isLoading,
+    setIsLoading,
+  };
+};
+
+export default useFirebase;
